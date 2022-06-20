72351c662f4a02ae3e55b6cce1e6ce9cfd2db5de
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-21 01:15:26 +0530

    LeepYearOrNot

---
+++ LeepYear.cs
@@ -0,0 +1,14 @@
+﻿
+
+// Print the year is a Leap Year or not.
+
+int Leep = 20000;
+if( Leep%4 ==0)
+{
+    Console.WriteLine("This is the Leep year");
+}
+
+else
+{
+    Console.WriteLine("This is not a Leep year");
+}
