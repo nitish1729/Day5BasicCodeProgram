53827f42e4e6f8bf219779a1beee340e20c41240
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-21 01:02:46 +0530

    FlipCoin

---
+++ flipCoin.cs
@@ -0,0 +1,17 @@
+﻿// Flip Coin and print percentage of Heads and Tails
+
+int Head = 0;
+
+Random prob = new Random();
+
+int Flip = prob.Next(0, 2);
+
+if (Head < 0.5)
+{
+    Console.WriteLine("Probability to get Head");
+
+}
+else
+{
+    Console.WriteLine("Probability to get Tail");
+}
