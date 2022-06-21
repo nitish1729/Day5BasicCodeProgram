2ae1232486abdba9e21bbb1a185004bfb4bdc786
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-21 22:44:04 +0530

    Check vowel or constant

---
+++ Nine_VowelOrCons.cs
@@ -0,0 +1,17 @@
+﻿        
+// Check Whether an Alphabet is Vowel or Consonant
+        char ch;
+ 
+        Console.WriteLine("Enter any character: ");
+        ch = Convert.ToChar(Console.ReadLine());
+ 
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+        {
+ 
+            Console.WriteLine(ch + " is Vowel.");
+ 
+        }
+        else 
+        {
+            Console.WriteLine(ch + " is Consonant.");            
+        }
