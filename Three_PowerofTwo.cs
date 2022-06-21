b56555b083977e1e973185d96d40cc3837c2d837
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-21 23:35:48 +0530

    powers of 2

---
+++ Three_PowerofTwo.cs
@@ -0,0 +1,8 @@
+﻿
+//powers of 2 that are less than or equal to 2^N.
+int n = 10; double result;
+		Console.WriteLine("Enter the value of N");
+	n = Convert.ToInt32(Console.ReadLine());
+		result = Math.Pow(2,n);
+	
+		Console.WriteLine("power of 2 : " + result);
