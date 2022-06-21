bdf132021d293827713dfaf771f110d7b8068cfe
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-21 22:17:34 +0530

    Whether Even or Odd

---
+++ Eight_EvenOrOdd.cs
@@ -0,0 +1,13 @@
+﻿        // Check Whether a Number is Even or Odd       
+         int n;
+		Console.WriteLine("Enter a Number");
+		n = Convert.ToInt32(Console.ReadLine());
+		if (n % 2 == 0 )
+		{
+		Console.WriteLine(n + " is even");
+		}
+		
+		else
+		{
+		Console.WriteLine(n + " is odd");
+		}
